0cfa41256926c55862423d221908980516e6dcc4
Carlos Augusto Mendoza Sanchez <cams.0093@gmail.com>
2016-03-02 16:17:36 -0600

    complete vector.c

--- vector.c
+++ vector.c
@@ -3,68 +3,129 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <search.h>
 
-void VectorNew(vector *v, int elemSize, VectorFreeFunction freeFn, int initialAllocation){
-	v->elems = malloc(elemSize *initialAllocation);
-	assert(v->elems != NULL && "There is no available memory"); //Si no se cumple hasta ahi llega el programa
+void VectorNew(vector *v, int elemSize, VectorFreeFunction freeFn, int initialAllocation)
+{
+	v->elems = malloc(elemSize*initialAllocation);
+	assert(v->elems != NULL && "No hay memoria");
 	v->elemSize = elemSize;
 	v->initialSize = initialAllocation;
 	v->currentPosition = 0;
 	v->freeFn = freeFn;
 }
-//freeFn destruir memoria
-void VectorDispose(vector *v){
-	if(v->freeFn != NULL){
+
+void VectorDispose(vector *v)
+{
+	if(v->freeFn!=NULL){
 		int i;
-		void *elemAddress;
-		elemAddress = v->elems
-		for(i=0; i<currentPosition; i++){
-			elemAddress = (char *)(v->elems)+i*v->elemSize;
+		void * elemAddress;
+		elemAddress = v->elems;
+		for(i=0; i<(v->currentPosition); i++){
+			elemAddress = (char *)v->elems + i * v->elemSize;
 			v->freeFn(elemAddress);
 		}
 	}
 	free(v->elems);
 }
 
-int VectorLength(const vector *v){ 
+int VectorLength(const vector *v)
+{
 	return v->currentPosition;
 }
 
-void *VectorNth(const vector *v, int position){ 
-	assert(position < v->currentPosition && "No valid position");
-	return (char *)(v->elems)+position*v->elemSize;
+void *VectorNth(const vector *v, int position)
+{
+	assert(position < v->currentPosition && "Posicion no valida");
+	return (char *)v->elems + position * v->elemSize;
 }
 
-void VectorReplace(vector *v, const void *elemAddr, int position){
-	void *elemdir;
-	assert(position < v->currentPosition && "No valid position");
-	elemdir = (char *)v->elems+position*v->elemSize;
-	memcpy(elemdir, elemAddr, v->elemSize);
+void VectorReplace(vector *v, const void *elemAddr, int position)
+{
+	assert(position < v->currentPosition && "Posicion no valida");
+	void * elemDir;
+	elemDir=(char *)v->elems + position * v->elemSize;
+	memcpy(elemDir, elemAddr, v->elemSize);
 }
 
-void VectorInsert(vector *v, const void *elemAddr, int position){
+void VectorInsert(vector *v, const void *elemAddr, int position)
+{
+	assert(position <= v->currentPosition && "Posicion no valida");
 	if(v->currentPosition == v->initialSize){
 		v->initialSize *= 2;
-		v->elems = realloc(v->elems, v->initialSize*v->elemSize);
-		asssert(v->elems != NULL, && "There is no available memory");
+		v->elems = realloc(v->elems, v->initialSize * v->elemSize);
+		assert(v->elems!=NULL && "No mas memoria para ti");
 	}
-	elemdir = (char *)v->elems+v->currentPosition*v->elemSize;
-	memcpy(elemdir, elemAddr, v->elemSize);
-	v->currentPosition +=1;
+
+	void * addr1 = (char *)v->elems + position * v->elemSize;
+	void * addr2 = (char *)addr1 + v->elemSize;
+	int size = (v->currentPosition - position) * v->elemSize;
+
+	memmove(addr2, addr1, size);
+
+	memcpy(addr1, elemAddr, v->elemSize);
+
+	v->currentPosition++;
 }
 
 void VectorAppend(vector *v, const void *elemAddr)
-{}
+{
+	void * elemDir;
+	if(v->currentPosition == v->initialSize){
+		v->initialSize *= 2;
+		v->elems = realloc(v->elems, v->initialSize * v->elemSize);
+		assert(v->elems!=NULL && "No mas memoria para ti");
+	}
+	elemDir = (char *)v->elems + v->currentPosition * v->elemSize;
+	memcpy(elemDir, elemAddr, v->elemSize);
+	v->currentPosition++;
+}
 
 void VectorDelete(vector *v, int position)
-{}
+{
+	assert(position < v->currentPosition && "Posicion no valida");
+	void * addr1 = (char *)v->elems + position * v->elemSize;
+	void * addr2 = (char *)addr1 + v->elemSize;
+	int size = (v->currentPosition - (position + 1)) * v->elemSize;
+
+	memmove(addr1, addr2, size);
+
+	v->currentPosition--;
+}
 
 void VectorSort(vector *v, VectorCompareFunction compare)
-{}
+{
+	qsort(v->elems, v->currentPosition, v->elemSize, compare);
+}
 
 void VectorMap(vector *v, VectorMapFunction mapFn, void *auxData)
-{}
+{
+	int i;
+	void * elemAddr;
+	for(i=0; i < v->currentPosition; i++){
+		elemAddr = (char *)v->elems + i * v->elemSize;
+		mapFn(elemAddr, auxData);
+	}
+}
 
 static const int kNotFound = -1;
 int VectorSearch(const vector *v, const void *key, VectorCompareFunction searchFn, int startIndex, bool isSorted)
-{ return -1; } 
+{
+	assert(startIndex < v->currentPosition && "Posicion no valida");
+	void *elemAddr;
+  	void *base = (char *) v->elems + startIndex * v->elemSize;
+  	size_t nmemb = v->currentPosition - startIndex;
+  	
+	if(isSorted){
+    	elemAddr = bsearch(key,base,nmemb,v->elemSize,searchFn);
+    }
+  	else{
+    	elemAddr = lfind(key,base,&nmemb,v->elemSize,searchFn);
+    }
+  
+  	if( elemAddr != NULL ){
+    	return ((char *)elemAddr - (char *)v->elems)/v->elemSize;
+    }
+
+  	return kNotFound;
+}
